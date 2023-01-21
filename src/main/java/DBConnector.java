ec050ed2639ed22bfd52fcfa70034a8d318d0c72
Zainab Lawal <zainablaw1012@gmail.com>
2023-01-21 16:20:10 +0300

    Added feature to save extracted judgements to db

--- src/main/java/DBConnector.java
+++ src/main/java/DBConnector.java
@@ -1,22 +1,32 @@
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class DBConnector {
 	static final String JDBC_Driver = "org.h2.Driver";
-	static final String DB_URL = "jdbc:h2:mem:test";
+	static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/rulings";
 
-	static final String USER = "zai";
-	static final String PASS = "";
+	static final String USER = "root";
+	static final String PASS = "Daddy123";
 	static Connection conn = null;
-	public static void main(String[] args) {
+	static PreparedStatement Pstmt;
+
+	public static PreparedStatement createConnection (){
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-			conn =DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Rulings" +
-							"user=root&password=Daddy123");
+			conn =DriverManager.getConnection(DB_URL,USER,PASS );
 
-			System.out.println("Connection is successful");
+			Pstmt= conn.prepareStatement("INSERT INTO judgements(id,title,basvuru_no,karar_tarihi,ruling_content)" + "values(?,?,?,?,?)");
+
+
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+		return Pstmt;
+	}
+	public static void main(String[] args) {
+
 	}
 }
--- src/main/java/Extractor.java
+++ src/main/java/Extractor.java
@@ -5,10 +5,14 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,40 +28,50 @@ public class Extractor {
 
 		return Integer.valueOf(lastPage);
 	}
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, SQLException, ParseException {
 		int numberOfPages = getNumberOfPages();
+		int j =1;
+		for (int i = 1; i < numberOfPages+1; i++) {
+			String url = "https://kararlarbilgibankasi.anayasa.gov.tr/?page="+i;
+			Document document = Jsoup.parse(new URL(url).openStream(), "CP1252", url);
+			Elements rulings = document.getElementsByClass("birkarar");
 
-		String url = "https://kararlarbilgibankasi.anayasa.gov.tr/?page=1";
-		Document document = Jsoup.parse(new URL(url).openStream(), "CP1252", url);
-		Elements rulings = document.getElementsByClass("birkarar");
-		for (Element ruling : rulings) {
-			System.out.println(ruling.text());
-			String rulingText = ruling.text();
+			PreparedStatement Pstmt = DBConnector.createConnection();
+			for (Element ruling:rulings) {
+				String rulingText = ruling.text();
+				System.out.println(rulingText);
 
-			List rulingmetaData = getMetadata(rulingText);
+				Map rulingmetaData = getMetadata(rulingText);
 
-			Element link = ruling.select("a[href]").get(0);
-			String judgementLink = print( "%s ",link.attr("abs:href"), trim(link.text(), 35));
+				Element link = ruling.select("a[href]").get(0);
+				String judgementLink = print( "%s ",link.attr("abs:href"), trim(link.text(), 35));
+
+				String rulingContent = getRulingDetails(judgementLink);
 
 
-			System.out.println(judgementLink);
-			System.out.println(getRulingDetails(judgementLink));
+
+				Pstmt.setInt(1,j);
+				Pstmt.setString(2,rulingmetaData.get("title").toString());
+				Pstmt.setString(3,rulingmetaData.get("kararNumber").toString());
+
+				String dateString = rulingmetaData.get("kararTarihi").toString();
+				System.out.println(dateString);
+				SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+				Date date = dateFormat.parse(dateString);
+
+
+				Pstmt.setDate(4, new java.sql.Date(date.getTime()));
+				Pstmt.setString(5,rulingContent);
+				Pstmt.addBatch();
+				j++;
+			}
+			Pstmt.executeBatch();
 		}
-		System.out.println(url);
-
-
-//		for (int i = 1; i < numberOfPages + 1; i++) {
-//			String url = "https://kararlarbilgibankasi.anayasa.gov.tr/?page="+i;
-//			Document document = Jsoup.parse(new URL(url).openStream(), "CP1252", url);
-//			Elements rulings = document.getElementsByClass("birkarar");
-//			for (Element ruling:rulings) {
-//				System.out.println(ruling.text());
-//			}
-//			System.out.println(url);
-//
-// 		}
-
 	}
+
+
+
+
 	private static String print(String msg, Object... args) {
 		return String.format(msg, args);
 	}
@@ -68,8 +82,8 @@ public class Extractor {
 		else
 			return s;
 	}
-	private static List getMetadata(String text) throws UnsupportedEncodingException {
-		List <String> metaData = new ArrayList(3);
+	private static Map getMetadata(String text) throws UnsupportedEncodingException {
+		Map metaData = new HashMap();
 
 		//convert encoding to UTF-8 to work with regex
 		text = new String(text.getBytes("CP1252"), "UTF-8");
@@ -78,36 +92,41 @@ public class Extractor {
 		Pattern titlePattern = Pattern.compile("^(.*?)\\s*Başvurusuna İlişkin Karar");
 		Matcher titleMatcher = titlePattern.matcher(text);
 		if (titleMatcher.find()) {
-			String title = new String(titleMatcher.group(1).getBytes("UTF-8"), "CP1252");
-			System.out.println("Title: " + title);
-			metaData.add(title);
+			String title = titleMatcher.group(1);
+			System.out.println(title);
+			metaData.put("title",title);
 		}
 		// Regular expression to match the number after "Karar Number"
 		Pattern kararNumberPattern = Pattern.compile("Başvurusuna İlişkin Karar\\s*(\\d+/\\d+)");
 		Matcher kararNumberMatcher = kararNumberPattern.matcher(text);
 		if (kararNumberMatcher.find()) {
 			String kararNumber = kararNumberMatcher.group(1);
-			System.out.println("Başvurusuna İlişkin Karar Number: " + kararNumber);
-			metaData.add(kararNumber);
+			System.out.println(kararNumber);
+			metaData.put("kararNumber",kararNumber);
+		}else{
+			Pattern kararNumberEnglishPattern = Pattern.compile("Başvurusuna İlişkin Karar English\\s*(\\d+/\\d+)");
+			Matcher kararNumberEnglishMatcher = kararNumberEnglishPattern.matcher(text);
+			if(kararNumberEnglishMatcher.find()) {
+				String kararNumber = kararNumberEnglishMatcher.group(1);
+				System.out.println(kararNumber);
+				metaData.put("kararNumber", kararNumber);
+			}
 		}
 		// Regular expression to match the number after "Karar Tarihi"
 		Pattern kararTarihiPattern = Pattern.compile("Karar Tarihi\\s*:\\s*(\\d{2}/\\d{2}/\\d{4})");
 		Matcher kararTarihiMatcher = kararTarihiPattern.matcher(text);
 		if (kararTarihiMatcher.find()) {
 			String kararTarihi = kararTarihiMatcher.group(1);
-			System.out.println("Karar Tarihi: " + kararTarihi);
-			metaData.add(kararTarihi);
+			System.out.println( kararTarihi);
+			metaData.put("kararTarihi",kararTarihi);
 		}
 
 		return metaData;
 	}
 	private static String getRulingDetails(String judgementLink) throws IOException {
-		//Document rulingDocument = Jsoup.parse(new URL(judgementLink).openStream(), "CP1252", judgementLink);
 		Document rulingDocument = Jsoup.connect(judgementLink).get();
 		Elements rulingDetails = rulingDocument.getElementsByClass("WordSection1");
-		String rulingDetailsText = new String(rulingDetails.text().getBytes("UTF-8"), "CP1252");
-
-		return rulingDetailsText;
+		return rulingDetails.text();
 	}
 
 }
